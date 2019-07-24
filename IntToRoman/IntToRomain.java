7bce4fca2d8bd1199ede336e7cfb89b02673e25a
Aman Ulla <35184363+connectaman@users.noreply.github.com>
2019-07-24 21:39:05 +0530

    Update IntToRomain.java

--- IntToRoman/IntToRomain.java
+++ IntToRoman/IntToRomain.java
@@ -8,37 +8,36 @@ public class IntToRomain {
             if(num >= 1000){
                 num = num - 1000;
                 roman = roman + "M";
-                continue;
+              
             }
             else if(num >= 500 && num < 1000){
                 num = num - 500;
                 roman = roman +"D";
-                continue;
+               
             }
             else if(num >= 100 && num < 500){
                 num = num - 100;
                 roman = roman + "C";
-                continue;
+             
             }
             else if(num >= 50 && num < 100){
                 num = num - 50;
                 roman = roman + "L";
-                continue;
+              
             }
             else if(num >= 10 && num < 50){
                 num = num - 10;
                 roman = roman + "X";
-                continue;
+                
             }
             else if(num >= 5 && num < 10){
                 num = num - 5;
-                roman = roman + "V";
-                continue;
+            
             }
             else{
                 num = num - 1;
                 roman = roman + "I";
-                continue;
+           
             }
         }
         return roman;
